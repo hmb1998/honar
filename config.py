b0545dc58842722a67575dc85584bb51b2d3e74a
hmb1998 <arekani341@gmail.com>
2026-08-22 09:44:57 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 PREFIX = "!"
 
 if not DISCORD_TOKEN:
-    raise RuntimeError("❌ DISCORD_TOKEN دانەنراوە لە Environment Variables.")
+    raise RuntimeError("❌ DISCORD_TOKEN دانەنراوە لە Environment Variables. لە Hosting ـەکەت Secret/Environment Variable ـێک بە ناوی DISCORD_TOKEN زیاد بکە.")
